60e107824c4f637f916d142122742372a580e567
Vaibhav Nagargoje <92326795+vaibhavnagargoje@users.noreply.github.com>
2023-10-31 20:10:10 +0000

    data types

---
+++ 01/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 24545
+let accountEmail="mail@gmail.com"
+var accountPassword ='234522'
+let accountState;
+accountCity='karmala'
+
+// accountId=343   // not allowed 
+accountEmail =" vai@email.com"
+accountPassword="vaafv"
+accountCity="pune"
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01/02_dataTypes.js
@@ -0,0 +1,21 @@
+" use strict"; // treat all JS code as a newer version of javascript by engine
+
+alert("Hello my name is Vaibhav "); // we are using nodejs not browser.
+
+// tc39.es  this is original doumentation 
+// mdn is also documentaion but this from mozilla organization 
+/*
+number= 2 to power 53
+bigint 
+string=" "
+boolen = true/flase 
+null =    (stand alone value )     Null is a object 
+undifined =>  // let num; not defined  Undifined is a type  
+sumbol =>  uniqe
+
+2) boject  
+console.log(type of 69)
+
+
+*/
+
