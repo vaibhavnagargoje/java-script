1d01d876b2fdbac25e1bc5ac4b25e6f7d119d779
Vaibhav Nagargoje <92326795+vaibhavnagargoje@users.noreply.github.com>
2023-11-01 05:50:45 +0000

    type conversion

---
+++ 01/03_conversion.js
@@ -0,0 +1,28 @@
+// let score="vaibhav"
+// console.log( typeof score)
+// let valInNumber=Number(score)
+// console.log(typeof valInNumber)
+// console.log(valInNumber);
+
+
+// "22" => 33
+// "34ag"=> NaN
+// true => 1  false=> 0
+
+
+// let isLoggedIn = " "
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn)
+//  //1 => true
+//  // " " => false
+//  // "vaibhav"=> true 
+
+
+
+let someNumber= 344
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+//  344=> "344"
