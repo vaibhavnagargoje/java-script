b10f1ad4f6005f76d0471a3350b9f3b26034cd3a
Vaibhav Nagargoje <92326795+vaibhavnagargoje@users.noreply.github.com>
2023-11-01 06:22:34 +0000

    04 _comparison operation

---
+++ 01/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log('3'>1)//true
+// console.log( null>0)  // false
+// console.log(null==0); // false
+// console.log(null>=0)  // true
+// console.log(0==0)     // true
+// console.log(undefined>0) // always false 
+
+
+console.log('3'===3)  // strictly or tightly  comparison // false 
+console.log('3'==3)   // losely comparison  // true 
