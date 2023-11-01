077a55b1aff3154198c89946fcc2adbcfd2d1981
Vaibhav Nagargoje <92326795+vaibhavnagargoje@users.noreply.github.com>
2023-11-01 06:10:39 +0000

    03_ how to conversion

--- 01/03_conversion.js
+++ 01/03_conversion.js
@@ -19,10 +19,50 @@
 
 
 
-let someNumber= 344
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber= 344
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
 
 //  344=> "344"
+
+
+
+
+
+
+
+
+
+
+//// opertions 
+
+
+// let value =3
+// let negvalue = -value
+// console.log(negvalue)
+
+// let str1 ="hello"
+// let str2=" Vaibhav"
+// let str3= str1+str2
+// console.log(str3);
+// console.log('2'+'4')   // 24
+// console.log('2'+4+5)   // 245
+// console.log(3+5+"3")   //83
+
+
+
+
+// console.log(true)   // true
+// console.log(+true) // 1
+// console.log(+" ")  // 0
+
+
+let num1,num2,num3
+num1=num2=num3=2+8
+console.log(num1)
+
+let gamecounter= 100
+gamecounter++
+console.log(gamecounter);  //101
