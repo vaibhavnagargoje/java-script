57170e763e3590d53a1e6005c76dd95f698f750e
vaibhav <vaibhavnagargoje2000@gmail.com>
2023-11-15 18:24:48 +0000

    key press tprinting with key code

---
+++ key_check/script.js
@@ -0,0 +1,24 @@
+const insert = document.getElementById('pressed')
+window.addEventListener('keydown',(e)=>{
+    insert.innerHTML= `
+    <div class='color'>
+        <table border>
+        
+            <tr>
+                <th> Key </th>
+                <th> Key Code</th>
+                <th> Code</th>
+
+            </tr>
+            <tr>
+                <td> ${e.key ===' '? "Space":e.key} </td>
+                <td> ${e.keyCode} </td>
+                <td> ${e.code} </td>
+
+            </tr>
+           
+
+        </table>
+    </div>
+    `
+})
