75d983a4416cbaf2952d512020b116f19d4b5762
Vaibhav Nagargoje <92326795+vaibhavnagargoje@users.noreply.github.com>
2024-02-05 13:00:32 +0000

    behind class using function constructor

--- objectOriantedProgramming/Calss&Constructor.js
+++ objectOriantedProgramming/Calss&Constructor.js
@@ -1,16 +1,31 @@
-class Test{
-    constructor(username,password,gmail){
-        this.username=username
-        this.password=password
-        this.gmail=gmail
-    }
+// class Test{
+//     constructor(username,password,gmail){
+//         this.username=username
+//         this.password=password
+//         this.gmail=gmail
+//     }
 
-    encryption(){
-        return ` username is ${this.username.toUpperCase()} and Encrypted Password is ${this.password}abc`;
-    }
+//     encryption(){
+//         return ` username is ${this.username.toUpperCase()} and Encrypted Password is ${this.password}abc`;
+//     }
+// }
+
+
+// let user1=  new Test("Vaibahv",'Vaibhav@gamil.com',"Pass@123")
+
+// console.log(user1.encryption())
+
+
+
+
+//behind the class 
+function User(username){
+    this.username=username;
 }
 
+User.prototype.Upper=function(){
+    return `${this.username.toUpperCase()}`
+}
 
-let user1=  new Test("Vaibahv",'Vaibhav@gamil.com',"Pass@123")
-
-console.log(user1.encryption())
+const vai= new User("Mauli")
+console.log(vai.Upper());
